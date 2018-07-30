d72e0feb0a7afb9e0c6019bb28b584c35758080d
杨俊宁 <1003719811@qq.com>
2018-07-30 17:42:40 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  base: '/realmRNDemo/',
+  title: 'realmRNDemo',
+  description: 'realm react native 中文文档',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    repo: 'youngjuning/realmRNDemo',
+    repoLabel: 'GitHub',
+    docsRepo: 'youngjuning/realmRNDemo',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: '帮助我们改善此页面！',
+    nav: [
+      { text: 'Getting Started', link: '/' },
+      { text: 'Introduce', link: '/introduce-realm-react-native/'},
+    ], // 导航栏
+    sidebar: 'auto', // 自动生成侧栏
+    lastUpdated: 'Last Updated', // 最后更新时间
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}
