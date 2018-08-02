320e183cdcc5882ad797173fa728432c23ab6135
杨俊宁 <1003719811@qq.com>
2018-08-02 17:03:23 +0800

    引入单例模式

--- src/common/RealmHelp.js
+++ src/common/RealmHelp.js
@@ -1,6 +1,30 @@
 import Realm from 'realm'
 
-export default class {
+/**
+ * 数组比较
+ * @param {*} arr1 数组1
+ * @param {*} arr2 数组2
+ */
+const equalArray = (arr1 = [], arr2 = []) => {
+  const result = arr1.sort().toString() === arr2.sort().toString()
+  return result
+}
+
+const init = (schema) => {
+  if (!RealmHelp.reaml) {
+    RealmHelp.realm = new Realm(schema)
+  } else if (!equalArray(RealmHelp.realm.schema, schema)) {
+    RealmHelp.realm.close()
+    RealmHelp.realm = new Realm({schema})
+  }
+  return RealmHelp.realm
+}
+
+export default class RealmHelp {
+  constructor(schema) {
+    this.realm = init(schema)
+  }
+
   static write = (config, callback) => {
     Realm.open(config).then((realm) => {
       realm.write(() => {
